aa1765513f4bfe352a15e683be5d146c94e50a4f
Evan_xr62 <nane33553@gmail.com>
2022-08-26 20:51:19 +0300

    Done4

--- Program.cs
+++ Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-           
+            List<int> _dataEx4 = new List<int> { -12, -33, 134, 12, 5, -3, 55, 185, 5, 58, 5, 10 };
+
+            var res = _dataEx4.Where(x => x > 0)
+                    .Select(x => x % 10)
+                    .Distinct()
+                    .ToList();
+            foreach (var item in res)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
         }
     }
 }
