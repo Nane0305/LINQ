00f380e558e72877324559e9d9bcb05eccf7184f
Evan_xr62 <nane33553@gmail.com>
2022-08-26 20:01:39 +0300

    Done

---
+++ Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    internal class Car
+    {
+        public string ? Name { get; set; }
+        public UInt32 ? Year { get; set; }
+        public decimal ? Price { get; set; }
+
+        public Car()
+        {
+            this.Name = null;
+            this.Year = null;
+            this.Price = null;
+        }
+        public Car(string Name, UInt32 Year, decimal Price) : this()
+        {
+            this.Name = Name;
+            this.Year = Year;
+            this.Price = Price;
+        }
+        public override string ToString()
+        {
+            return $"Name:{Name} \nYear:{Year} \nPrice:{Price}";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,34 @@
-﻿namespace LINQ
+﻿
+using System;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+
+namespace LINQ
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-           
+            Car[] cars = new Car[]
+            {
+                new Car("Audi", 2005, 5000),
+                new Car("BMW", 1999, 3000),
+                new Car("Toyota", 2020, 304004),
+                new Car("Mercedes", 2020, 4454545),
+                new Car("Nisan", 2012, 12000),
+                new Car("ZAZ", 2006, 8056)
+            };
+            var qwery = from car in cars
+                        group car by car.Year > 2010;
+            foreach (var item in qwery)
+            {
+                Console.Write($"Key: {item.Key}\nValue:");
+                foreach (Car item2 in item)
+                {
+                    Console.Write($"\t{item2}");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
