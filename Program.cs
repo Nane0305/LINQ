ce3ce0299b8efe437b23ee0c00bdf178c438f699
Evan_xr62 <nane33553@gmail.com>
2022-08-26 20:31:25 +0300

    Done2

--- Program.cs
+++ Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-           
+            int D = 5;
+            List<int> list1 = new List<int> { -10, 21, 35, 9, 10, -33, 40, 165, -25 };
+
+            var result_1 = list1.FirstOrDefault(x => (x > 0) && (x % 10 == D));
+
+            Console.WriteLine(result_1);
+            Console.ReadKey();
         }
     }
 }
