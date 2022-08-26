2dc1c9c0465f7199af1eb0cc1491c4b29a454a19
Evan_xr62 <nane33553@gmail.com>
2022-08-26 20:49:23 +0300

    Done3

--- Program.cs
+++ Program.cs
@@ -1,10 +1,28 @@
-﻿namespace LINQ
+﻿using System.Linq;
+
+namespace LINQ
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-           
+            int L = 3;
+            List<char> text = new List<char>("qwery12435");
+
+            IEnumerable<char> buf = ((from sign in text
+                                      where char.IsDigit(sign)
+                                      select sign).Take(L)) ?? "Not Found";
+
+
+            string? res = new string(buf.Take(buf.Count()).ToArray());
+            if (res.Length <= 1)
+            {
+                Console.WriteLine("Not Found");
+            }
+            else
+            {
+                Console.WriteLine(res);
+            }
         }
     }
 }
